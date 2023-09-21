65118ce3817cc80691d54ac1b7a4915af43cdf5e
Tom Jeannesson <tomjeannesson@gmail.com>
2023-09-21 14:51:40 +0200

    fix(ci): pypi

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author="Mateo & Tom Jeannesson",
     author_email="tomjeannesson@gmail.com",
     url="https://github.com/tomjeannesson/result-analytics",
-    download_url="https://github.com/tomjeannesson/project-results/archive/refs/tags/v{{VERSION}}.tar.gz",
+    download_url="https://github.com/tomjeannesson/result-analytics/archive/refs/tags/v{{VERSION}}.tar.gz",
     keywords=["Sport", "Ski", "Result", "Analytics", "Moguls"],
     install_requires=["PyPDF2==3.0.1", "pandas==2.1.1"],
     classifiers=[
